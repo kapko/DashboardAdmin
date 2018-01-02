eb71a70a62c39d6299699739b242f98a312587c2
kzhekshen <kzhekshen@incentient.com>
2018-01-02 14:30:09 +0600

    google maps fixed

--- src/app/posts/create/post-create.component.ts
+++ src/app/posts/create/post-create.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, NgZone, ElementRef, ViewChild } from '@angular/core';
 import { Post } from '../../interfaces/post.interface';
 import { PostService } from 'app/posts/post.service';
 import { Promise } from 'firebase/app';
 import { forSell, typeOfRooms, typeOfFixes, typeOfPlan, typeOfCurrency } from '../post';
 import createNumberMask from 'text-mask-addons/dist/createNumberMask';
+import { MapsAPILoader } from '@agm/core';
 
 export interface ImageStructure {
   fileName: string;
@@ -12,10 +13,13 @@ export interface ImageStructure {
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: 'post-create.html'
+  templateUrl: 'post-create.html',
 })
 
 export class PostCreateComponent {
+  @ViewChild('search')
+  public searchElementRef: ElementRef;
+
   // DEFAULT OPTIONS
   forSell = forSell;
   typeOfRooms = typeOfRooms;
@@ -46,9 +50,55 @@ export class PostCreateComponent {
 
   numberMask = createNumberMask({prefix: '', integerLimit: 9});
 
+  // google maps
+  public lat: number;
+  public lng: number;
+  public zoom: number = 17;
+
   constructor(
     private postService: PostService,
-  ) {}
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {
+    //set google maps defaults
+    this.lat = 42.8746212;
+    this.lng = 74.5697617;
+  }
+
+  mapClicked(event: any) {
+    this.lat = event.coords.lat;
+    this.lng = event.coords.lng;
+  }
+
+  markerDragEnd($event: MouseEvent) {
+    console.log('dragEnd', $event);
+  }
+
+
+  ngOnInit() {
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ['address'],
+        componentRestrictions: {country: "kgz"}
+      });
+
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+  
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          //set lat, lng and zoom
+          this.lat = place.geometry.location.lat();
+          this.lng = place.geometry.location.lng();
+        });
+      });
+    }); // mapsAPILoader
+  }
 
   // upload files
   onUploadFinished(event: any): void {
--- src/app/posts/post.module.ts
+++ src/app/posts/post.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 import { PostListComponent } from 'app/posts/posts-list/posts-list.component';
 import { PostsRoutingModule } from 'app/posts/post.router';
@@ -13,6 +14,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 // services
 import { PostService } from 'app/posts/post.service';
 import { TextMaskModule } from 'angular2-text-mask';
+import { GooglePlaceModule } from 'angular2-google-place';
 
 const config = {
   apiKey: 'AIzaSyDlvb_n_HrLNv84m6WE8SdrMU9LP5-88uU',
@@ -32,6 +34,10 @@ const config = {
     ImageUploadModule.forRoot(),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBFEC9dv1dCcp9TvfXuoinfL2B_IYJSPz0',
+      libraries: ['places']
+    }),
     TextMaskModule,
     AngularFireModule.initializeApp(config),
   ],
